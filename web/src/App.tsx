1b489610fce6e2a762f8336728f861b931c25184
Diego Rodrigo <diegorodrigo90@gmail.com>
2020-10-13 17:33:18 -0300

    Landing done

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+
+import './styles/global.css';
+
+
+
+function App() {
+  return (
+    <div></div>
+    );
+  }
+  
+  export default App;
