fbee2cd7d3db9c48c33d45cc0151c0a65ffae18d
Diego Rodrigo <diegorodrigo90@gmail.com>
2020-10-13 22:25:28 -0300

    finish class one

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
-
-
 import './styles/global.css';
 
+import Routes from './routes';
 
 
 function App() {
-  return (
-    <div></div>
-    );
+  return <Routes />;
   }
   
   export default App;
